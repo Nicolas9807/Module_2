8f8fa8b46ab846592d8b03d51f6ed45b45722daa
Nicolas9807 <nikolasstolyar18@gmail.com>
2020-12-02 16:46:25 +0200

    Created JUnit test for appendFormatted method.

---
+++ src/test/java/sample/ShoppingCartAppendFormatted.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Stolyar Mykola
+ */
+
+public class ShoppingCartAppendFormatted {
+
+    /**
+     * Test of appendFormatted method, of class ShoppingCart.
+     */
+    @Test
+    public void testAppendFormatted() {
+        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "123451", -1, 5);
+        assertEquals(sb.toString(), "12345 ");
+    }
+}
